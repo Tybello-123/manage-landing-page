d3aa4105e6f77d5a6b37d7a17d79aa0487060449
toybat <applicationstoybatbello@gmail.com>
2023-07-12 10:19:53 +0100

    minor changes

--- script.js
+++ script.js
@@ -7,11 +7,11 @@ btn.addEventListener('click', () => {
     nav.classList.toggle('hidden')
 })
 
-const formEl = document.getElementById("form");
+const newsletterForm = document.getElementById("form");
 let newsletterEl = document.getElementById("newsletter");
 
 
-formEl.addEventListener("submit", e => {
+newsletterForm.addEventListener("submit", e => {
     e.preventDefault();
     console.log("submit");
 })
@@ -21,62 +21,148 @@ formEl.addEventListener("submit", e => {
 
 //displaying the login modals for the sections
 
-const loginSection = document.getElementById("login-modal")
+const signUpSection = document.getElementById("signup-modal")
+
+// document.querySelectorAll('#getStarted','#getStarted2').addEventListener('click', () => {
+  
+//   signUpSection.classList.add("block");
+//   signUpSection.classList.remove("hidden");
+// })
 
 document.getElementById('getStarted').addEventListener("click", () => {
 
-  loginSection.classList.add("block");
-  loginSection.classList.remove("hidden");
+  signUpSection.classList.add("block");
+  signUpSection.classList.remove("hidden");
 })
 
 document.getElementById('getStarted2').addEventListener("click", () => {
 
-  loginSection.classList.add("block");
-  loginSection.classList.remove("hidden");
+  signUpSection.classList.add("block");
+  signUpSection.classList.remove("hidden");
 })
 
 document.getElementById('getStarted3').addEventListener("click", () => {
 
-  loginSection.classList.add("block");
-  loginSection.classList.remove("hidden");
+  signUpSection.classList.add("block");
+  signUpSection.classList.remove("hidden");
 })
 
 document.getElementById('getStarted4').addEventListener("click", () => {
 
-  loginSection.classList.add("block");
-  loginSection.classList.remove("hidden");
+  signUpSection.classList.add("block");
+  signUpSection.classList.remove("hidden");
 })
 
-document.getElementById('close-btn').addEventListener("click", () => {
-  loginSection.classList.remove("block");
-  loginSection.classList.add("hidden");
+document.getElementById('signup-close').addEventListener("click", () => {
+  signUpSection.classList.remove("block");
+  signUpSection.classList.add("hidden");
 })
 
 
 //implemting the password button
 const passwordInput = document.getElementById('password');
-const eye = document.getElementById("eye")
-
+const eye = document.getElementById("eye");
 
 eye.addEventListener("click", function(){
   this.classList.toggle("fa-eye-slash")
-
-  const type = passwordInput.getAttribute("type") === "password" ? 
-  "text": "password"
-  passwordInput.setAttribute("type",type);
-  
+  const type = passwordInput.getAttribute("type") === "password" ? "text" : "password"
+  passwordInput.setAttribute("type", type)
 })
 
 
 
 //using swiper js 
 
-    var swiper = new Swiper(".mySwiper", {
-      slidesPerView: "auto",
-      spaceBetween: 0,
-      freeMode: true,
-      pagination: {
-        el: ".swiper-pagination",
-        clickable: true,
-      },
-    });
+var swiper = new Swiper(".mySwiper", {
+  slidesPerView: "auto",
+  spaceBetween: 0,
+  freeMode: true,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+});
+
+
+// const loginModal = document.getElementById('login-modal')
+// //login element
+// document.getElementById('login-modal').addEventListener('click' , () => {
+//   signUpSection.remove();
+  
+//   loginModal.classList.remove = "hidden"
+//   loginModal.classList.add ="block"
+  
+
+
+// })
+
+
+// // //form validation
+// const username = document.getElementById('name');
+// const email = document.getElementById('email');
+// const password = document.getElementById('password');
+
+
+
+// const isValidEmail = email => {
+//   const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+//   return re.test(String(email).toLowerCase());
+// }
+
+// signUpSection.addEventListener('submit', e => {
+//   e.preventDefault();
+
+//   validateInputs();
+// })
+
+// const setError = (element, message) => {
+//   const inputControl = element.parentElement;
+//   const errorDisplay = inputControl.querySelector('.error');
+
+
+//   errorDisplay.innerText = message;
+//   inputControl.classList.remove('success');
+//   inputControl.classList.add('error');
+
+// }
+
+// const setSuccess = element => {
+//   const inputControl = element.parentElement;
+//   const errorDisplay = inputControl.querySelector(".error");
+
+//   errorDisplay.innerText = "";
+//   inputControl.classList.add('success');
+//   inputControl.classList.remove('error');
+
+// }
+
+// const validateInputs = () => {
+//   const usernameValue = username.value.trim();
+//   const emailValue = email.value.trim();
+//   const passwordValue = password.value.trim();
+
+
+//   if(usernameValue === ''){
+//     setError(username, 'Username is required');
+//   }else{
+//     setSuccess(username);
+//   }
+
+//   if(emailValue === ''){
+//     setError(email, 'Email is required');
+//   }else if(!isValidEmail(emailValue)) {
+//     setError(email, 'Provide email address');
+//   }else{
+//     setSuccess(email);
+//   }
+
+//   if(passwordValue = ''){
+//     setError(password, 'password is required')
+//   }else if (passwordValue.length < 8){
+//     setError(password,'Password must be at least 8 characters')
+//   }else{
+//     setSuccess(password)
+//   }
+
+// }
+
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./manage-landing-page/*html'],
+  content: ['./*html'],
   theme: {
     screens:{
       sm: '350px',
